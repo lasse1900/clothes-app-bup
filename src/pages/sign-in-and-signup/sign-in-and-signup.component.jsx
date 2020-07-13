c5f774ba3c2c9044683c139639a8f2732599a73a
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-07-13 15:04:55 +0300

    added formInput component

--- src/pages/sign-in-and-signup/sign-in-and-signup.component.jsx
+++ src/pages/sign-in-and-signup/sign-in-and-signup.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './sign-in-and-signup.styles.sass';
+import './sign-in-and-signup.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
 
 const SignIAndSignUpPage = () => (
