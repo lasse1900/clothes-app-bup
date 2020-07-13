64055e2e5f0a9b8598edd88d5a29e960d0eec955
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-07-13 13:06:12 +0300

    added SignIn components

---
+++ src/pages/sign-in-and-signup/sign-in-and-signup.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './sign-in-and-signup.styles.sass';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignIAndSignUpPage = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
+
+);
+
+
+export default SignIAndSignUpPage;
