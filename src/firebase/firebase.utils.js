aee8d5d9fff61f42a616972edb033c4f15ace93b
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-09-08 18:29:04 +0300

    added shop data to redux direct from firebase/firestore - update shop data to see long ids from console shop/collections

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -68,8 +68,13 @@ export const convertCollectionsSnapshotToMap = (collections) => {
     };
   });
 
-  console.log(transformedCollection)
-}
+  // console.log(transformedCollection)
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CollectionPage from '../collection/collection.component';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
+import { updateCollections } from '../../redux/shop/shop.actions';
+
 class ShopPage extends React.Component {
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
+    const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
     collectionRef.onSnapshot(async snapshot => {
       // console.log(snapshot);
-      convertCollectionsSnapshotToMap(snapshot);
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      // console.log(collectionsMap);
     })
   }
 
@@ -29,5 +35,9 @@ class ShopPage extends React.Component {
   }
 }
 
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap =>
+    dispatch(updateCollections(collectionsMap))
+})
 
-export default ShopPage;
+export default connect(null, mapDispatchToProps)(ShopPage);
