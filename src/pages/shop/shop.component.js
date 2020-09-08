4bfa5a9701a54e6bd0aa81c597915adc16b03101
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-09-08 13:21:22 +0300

    bringing shop data from firebase to App

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -4,12 +4,30 @@ import { Route } from 'react-router-dom';
 import CollectionPage from '../collection/collection.component';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-const ShopPage = ({ match }) => (
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
-  <div className='shop-page'>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection('collections');
+
+    collectionRef.onSnapshot(async snapshot => {
+      // console.log(snapshot);
+      convertCollectionsSnapshotToMap(snapshot);
+    })
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+      </div>
+    );
+  }
+}
+
 
 export default ShopPage;
