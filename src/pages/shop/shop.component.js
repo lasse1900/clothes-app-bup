888b47ee22fdd9f4d5ff1747817472bcf7794193
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-09-09 19:08:11 +0300

    debugging code and fixex a bug (go to e.g. from main page to Hats and refresh, that causcaused a bug which was fixed)

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -7,7 +7,7 @@ import CollectionPage from '../collection/collection.component';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
-import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
+import { selectIsCollectionFetching, selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
 
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
@@ -22,19 +22,28 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    const { match, isCollectionFetching } = this.props;
+    const { match, isFetchingCollections, isCollectionLoaded } = this.props;
 
     return (
       <div className='shop-page'>
-        <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={isCollectionFetching} {...props} />} />
-        <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props} />} />
+        <Route exact path={`${match.path}`} render={(props) =>
+          <CollectionOverviewWithSpinner
+            isLoading={isFetchingCollections}
+            {...props} />}
+        />
+        <Route path={`${match.path}/:collectionId`} render={(props) =>
+          <CollectionPageWithSpinner
+            isLoading={!isCollectionLoaded}
+            {...props} />}
+        />
       </div>
     );
   }
 }
 
 const mapStateToProps = createStructuredSelector({
-  isCollectionFetching: selectIsCollectionFetching
+  isFetchingCollections: selectIsCollectionFetching,
+  isCollectionLoaded: selectIsCollectionsLoaded
 });
 
 const mapDispatchToProps = dispatch => ({
