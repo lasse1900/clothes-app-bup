f3813e585fb2cd9dbcc3b6093a29460d62b62e6d
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-09-18 11:26:50 +0300

    return to previous stage without hook in collection.component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
@@ -7,21 +7,7 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import { CollectionPageContainer, CollectionTitle, CollectionItemsContainer } from './collection.styles';
 
-import { firestore } from '../../firebase/firebase.utils';
-
 const CollectionPage = ({ collection }) => {
-
-  useEffect(() => {
-    console.log('I am subscribing');
-    const unsubscribeFromCollections = firestore
-      .collection('collections')
-      .onSnapshot(snapshot => console.log(snapshot));
-
-    return () => {
-      console.log('I am unsubscribing');
-      unsubscribeFromCollections();
-    };
-  }, []);
 
   const { title, items } = collection;
   return (
